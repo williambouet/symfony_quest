328b1eec6d7ed55d35853d6c040a9efee188df3d
WILLIAM BOUET <william.bouet.14@gmail.com>
2022-11-27 20:51:02 +0100

    Quest05 done

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,11 +6,18 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+#[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
-    #[Route('/program/', name: 'program_index')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
         return $this->render('program/index.html.twig', ['website' => 'Wild Series',]);
+    }
+
+    #[Route('/{id}', requirements: ['id' => '\d+'], methods: ['GET'], name: 'id')]
+    public function show(int $id = 1): Response
+    {
+        return $this->render('program/show.html.twig', ['id' => $id]);
     }
 }
