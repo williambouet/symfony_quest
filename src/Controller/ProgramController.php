65d3e4923505cf196969cbd679358f082454bcbf
WILLIAM BOUET <william.bouet.14@gmail.com>
2022-11-30 20:14:18 +0100

    try to fix season pb

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -36,21 +36,19 @@ class ProgramController extends AbstractController
 
 
     #[Route('/{id}', requirements: ['id' => '^[0-9]+$'], methods: ['GET'], name: 'show')]
-    public function show(int $id, ProgramRepository $programRepository): Response
-    {
-        $program = $programRepository->findOneBy(['id' => $id]);
-
+    public function show(Program $program): Response
+    { //avec la méthode magique ^
+              $seasons = $program->getSeasons();
         if (!$program)
             throw $this->createNotFoundException('Aucune série trouvée');
 
-        return $this->render('program/show.html.twig', ['program' => $program]);
+        return $this->render('program/show.html.twig', ['program' => $program, 'seasons' => $seasons]);
     }
 
     #[Route('/program/{programId}/seasons/{seasonId}', requirements: ['seasonId' => '^[0-9]+$', 'programId' => '^[0-9]+$',], methods: ['GET'], name: 'season_show')]
     public function showSeason(int $programId, int $seasonId, ProgramRepository $programRepository, SeasonRepository $seasonRepository): Response
     {
         $program = $programRepository->findOneBy(['id' => $programId]);
-
         if (!$program)
             throw $this->createNotFoundException('Aucune série trouvée');
 
