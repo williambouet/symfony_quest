7da6340aa205d08dd830070799f38a1ae44109c2
WILLIAM BOUET <william.bouet.14@gmail.com>
2022-12-01 14:15:06 +0100

    Quest14 done

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -7,14 +7,19 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 
 
 class CategoryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder->add('name', TextType::class);
-        $builder->add('name', TextType::class);
+        $builder->add('name', TextType::class, [
+            'label'    => 'Entrez le nom d\'une nouvelle catégorie',
+            'required' => true,
+        ]);
+
+        
     }
 
     public function configureOptions(OptionsResolver $resolver): void
